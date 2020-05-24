1e355146bd7fc9c95ea847ef62d3ee65252553f6
jtlemkin <jtlemkin@gmail.com>
2020-05-24 14:14:43 -0700

    Initial Commit

---
+++ BluetoothPeripheral/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BluetoothPeripheral
+//
+//  Created by James Lemkin on 5/24/20.
+//  Copyright © 2020 James Lemkin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
